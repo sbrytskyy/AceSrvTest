22973132e05e3b55e0603c3008a09ad4a1893f92
sbrytskyy <sbrytskyy@gmail.com>
2015-02-20 15:45:34 +0200

    Added ACE_SignalHandler

--- AceSrvTest/ServerMain.cpp
+++ AceSrvTest/ServerMain.cpp
@@ -1,10 +1,19 @@
 #include <ace/ACE.h>
+#include "ace/Signal.h"
 
 #include "ChatServer.h"
+
+namespace {
+	extern "C" void sigterm_handler(int /* signum */) { /* No-op. */ }
+}
+
 
 int ACE_TMAIN(int, ACE_TCHAR *[])
 {
 	Util::log("%s\n", "[Server] START");
+
+	// Register to receive the <SIGTERM> signal.
+	ACE_Sig_Action sa((ACE_SignalHandler)sigterm_handler, SIGTERM);
 
 	ChatServer chatServer;
 	chatServer.run();
